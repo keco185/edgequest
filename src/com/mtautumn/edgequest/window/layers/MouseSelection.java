e0acc3022c662454af832d2f6bba1a50d7daf245
keco185 <keco185@me.com>
2017-01-06 15:58:09 -0500

    Hides mouse selection when aiming

--- src/com/mtautumn/edgequest/window/layers/MouseSelection.java
+++ src/com/mtautumn/edgequest/window/layers/MouseSelection.java
@@ -3,18 +3,20 @@ package com.mtautumn.edgequest.window.layers;
 import com.mtautumn.edgequest.window.Renderer;
 
 public class MouseSelection {
-	public static void draw(Renderer r) {		
-		float posX = getMousePosX(r);
-		float posY = getMousePosY(r);
-		float blockSize = r.dataManager.settings.blockSize;
-		
-		if (r.dataManager.system.isMouseFar) drawFarSelection(r, posX, posY, blockSize);
-		else drawNearSelection(r, posX, posY, blockSize);
-		
-		if (r.dataManager.system.isKeyboardTravel) drawFlag(r, posX, posY, blockSize);
+	public static void draw(Renderer r) {
+		if (!r.dataManager.system.isAiming) {
+			float posX = getMousePosX(r);
+			float posY = getMousePosY(r);
+			float blockSize = r.dataManager.settings.blockSize;
+
+			if (r.dataManager.system.isMouseFar) drawFarSelection(r, posX, posY, blockSize);
+			else drawNearSelection(r, posX, posY, blockSize);
+
+			if (r.dataManager.system.isKeyboardTravel) drawFlag(r, posX, posY, blockSize);
+		}
 	}
-	
-	
+
+
 	private static float getMousePosX(Renderer r) {
 		double coordsOffsetX = offsetX(r);
 		return (float)((r.dataManager.system.mouseX - coordsOffsetX)*r.dataManager.settings.blockSize);
@@ -23,15 +25,15 @@ public class MouseSelection {
 		double coordsOffsetY = offsetY(r);
 		return (float)((r.dataManager.system.mouseY - coordsOffsetY)*r.dataManager.settings.blockSize);
 	}
-	
-	
+
+
 	private static double offsetX(Renderer r) {
 		return r.dataManager.system.screenX - Double.valueOf(r.dataManager.settings.screenWidth) / Double.valueOf(2 * r.dataManager.settings.blockSize);
 	}
 	private static double offsetY(Renderer r) {
 		return r.dataManager.system.screenY - Double.valueOf(r.dataManager.settings.screenHeight) / Double.valueOf(2 * r.dataManager.settings.blockSize);
 	}
-	
+
 	private static void drawFarSelection(Renderer r, float posX, float posY, float blockSize) {
 		r.drawTexture(r.textureManager.getTexture("selectFar"), posX, posY, blockSize, blockSize);
 
