91548121b3d53f7d1464aa6beed0b63d2a77a922
keco185 <keco185@me.com>
2017-01-17 20:36:17 -0500

    Damage Posts now more visible

--- src/com/mtautumn/edgequest/window/layers/DamagePosts.java
+++ src/com/mtautumn/edgequest/window/layers/DamagePosts.java
@@ -18,6 +18,11 @@ public class DamagePosts {
 		double blockSize = r.dataManager.settings.blockSize;
 		float pixelsX = (float) ((x - (r.dataManager.system.screenX - (Double.valueOf(r.dataManager.settings.screenWidth)/2.0)/blockSize))*blockSize);
 		float pixelsY = (float) ((y - (r.dataManager.system.screenY - (Double.valueOf(r.dataManager.settings.screenHeight)/2.0)/blockSize))*blockSize);
-		r.damageFont.drawString(pixelsX, pixelsY, Integer.toString(damage), Color.red);
+		float stringWidth = r.damageFont.getWidth(Integer.toString(damage));
+		float stringHeight = r.damageFont.getHeight(Integer.toString(damage));
+		float imageSize = (stringWidth > stringHeight) ? stringWidth : stringHeight;
+		imageSize *= 1.25f;
+		r.drawTexture(r.textureManager.getTexture("bloodSpatter"), pixelsX - (imageSize - stringWidth) / 2f, pixelsY, imageSize, imageSize);
+		r.damageFont.drawString(pixelsX, pixelsY, Integer.toString(damage), Color.white);
 	}
 }
